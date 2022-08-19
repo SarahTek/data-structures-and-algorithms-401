95300a8bca308d37e698483cf80d235f0798846d
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-08-18 23:15:24 -0400

    starter code

---
+++ javascript/SetOfStacks/stacks.ts
@@ -0,0 +1,72 @@
+export class SetOfStacks<T> {
+  private stacks: Stack<T>[] = [];
+
+  // Must use the below Stack class internally.
+  // When you need to create a new stack, do so with:
+  //
+  //    let stack = new Stack<T>(this.maxHeight);
+  //
+  constructor(private readonly maxHeight: number) {}
+
+  push(t: T): void {
+    if (this.stacks.length === 0) {
+      this.stacks.push(new Stack<T>(this.maxHeight));
+    }
+    let top = this.stacks.at(-1)!;
+    if (top.size === this.maxHeight) {
+      top = new Stack<T>(this.maxHeight);
+      this.stacks.push(top);
+    }
+    top.push(t);
+  }
+
+  pop(): T | undefined {
+    let top = this.stacks.at(-1);
+    if (top) {
+      let t = top.pop();
+      if (top.size === 0) {
+        this.stacks.pop();
+      }
+      return t;
+    }
+    return undefined;
+  }
+
+  get peek(): T | undefined {
+    let top = this.stacks.at(-1);
+    if (top) {
+      return top.peek;
+    }
+    return undefined;
+  }
+
+  // BONUS QUESTION
+  get size(): number {
+    return this.stacks.reduce((c, s) => c + s.size, 0);
+  }
+}
+
+class Stack<T> {
+  readonly _arr: T[] = [];
+
+  constructor(private readonly maxHeight: number) {}
+
+  push(t: T) {
+    if (this._arr.length > this.maxHeight) {
+      throw new Error("Stack toppled over!");
+    }
+    this._arr.push(t);
+  }
+
+  pop() {
+    return this._arr.pop();
+  }
+
+  get peek(): T | undefined {
+    return this._arr.at(-1);
+  }
+
+  get size(): number {
+    return this._arr.length;
+  }
+}
