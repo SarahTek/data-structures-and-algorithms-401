778225befb63ea3c2d33738774f43cd92fc10bf6
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-09-05 23:48:12 -0400

    working on BST class

--- javascript/Trees/src/binary_search_tree.js
+++ javascript/Trees/src/binary_search_tree.js
@@ -1,7 +1,13 @@
 const { Node, BinaryTree } = require("../src/binary_tree");
 
 class BinarySearchTree extends BinaryTree {
-  add(number) { }
+  add(number) { 
+    let current = this.root;
+    let newNode = new Node(number);
+    if (!this.root){
+    this.root = newNode
+  }
+}
   contains(number) { }
 }
 
